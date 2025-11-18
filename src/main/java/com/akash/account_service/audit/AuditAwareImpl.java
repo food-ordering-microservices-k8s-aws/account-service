142185a2f6e7e5205baf255d9d6f14cdf531feb7
Akash <akash.bhuiyan@gmail.com>
2025-11-18 16:26:02 +0100

    auditor name changed.

--- src/main/java/com/akash/account_service/audit/AuditAwareImpl.java
+++ src/main/java/com/akash/account_service/audit/AuditAwareImpl.java
@@ -13,6 +13,6 @@ import java.util.Optional;
 public class AuditAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
-        return Optional.of("restaurant-onboarding-service");
+        return Optional.of("account-service");
     }
 }
